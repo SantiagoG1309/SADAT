7b1cd80d91e0b969f26e879bace098bd73ebecf0
SantiagoG1309 <kevxn_garcxa@outlook.com>
2025-04-12 21:51:31 -0500

    Primer commit

--- sadat/settings.py
+++ sadat/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environmet.get('SECRET_KEY', default'sdfds66fs6d6f6ds6f6')    
+SECRET_KEY = os.environ.get('SECRET_KEY', default'sdfds66fs6d6f6ds6f6')    
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
