07ca37d101ad17e0342407adb65c89430973641b
SantiagoG1309 <kevxn_garcxa@outlook.com>
2025-04-12 21:54:22 -0500

    Primer commit

--- sadat/settings.py
+++ sadat/settings.py
@@ -14,12 +14,12 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+import os
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default'sdfds66fs6d6f6ds6f6')    
+SECRET_KEY = os.environ.get('SECRET_KEY', default='sdfds66fs6d6f6ds6f6')    
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
